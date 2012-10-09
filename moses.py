7e8c30a501c45e9ef0db21543b1867aeb78b2f52
Kat Busch <kbusch@stanford.edu>
2012-10-08 19:33:09 -0700

    updated moses script

--- moses.py
+++ moses.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import time
+import commands
+import subprocess
 
 HOME = "/afs/ir/users/k/b/kbusch/cs224n/pa1-mt"
 MOSES = "/afs/ir/class/cs224n/bin/mosesdecoder"
@@ -9,13 +11,19 @@ GIZA = "/afs/ir/class/cs224n/bin/giza-pp-read-only/external-bin-dir"
 MERT = ""
 PRO = "--pairwise-ranked"
 
-def run_command(name, command):
+def run_command(name, command, stdin=None, stdout=None):
 	outfile.write('Running %s\n' %(name))
 	outfile.write('Command: \n %s' % (command))
+	outfile.flush()
 	start = time.time()
-	os.system(command)
+	#output = commands.getoutput(command)
+	if stdin or stdout:
+		subprocess.call(command, stdin=stdin, stdout=stdout)
+	else:
+		os.system(command)
 	end = time.time()
-	outfile.write('\nRuntime: %d'%(end - start))
+	outfile.write('\nRuntime: %d\n\n'%(end - start))
+	outfile.flush()
 
 def gen_phrase_table(phrase_len):
 	command = ('%s/scripts/training/train-model.perl --max-phrase-length %d '
@@ -36,26 +44,31 @@ def tune(dist_lim, alg):
 	run_command("tuning", command) 
 
 def decode(name):
-	command = ('cat %s/mt-dev-test.fr | %s/bin/moses -du '
-		'-f %s/tune/moses.ini > %s/output/%s-test.out' %
-		(HOME, MOSES, HOME, HOME, name) )
-	run_command("decoding", command)
-	os.system("cp %s/tune/moses.ini %s/output/%s-moses.init"%(HOME, HOME, name))
+	command = ['%s/bin/moses'%(MOSES), '-du',
+		'-f', '%s/tune/moses.ini'%(HOME)]
+	run_command("decoding", command, 
+		stdin=open('%s/mt-dev-test.fr'%(HOME)),
+		stdout=open('%s/output/%s-test.out'%(HOME, name), 'w'))
+
+	os.system("cp %s/tune/moses.ini %s/output/%s-moses.ini"%(HOME, HOME, name))
 
 def eval(name):
-	command = ('%s/scripts/generic/multi-bleu.perl '
-	 	'%s/mt-dev-test.en < %s/output/%s-test.out > %s/output/%s-eval.out' %
-	 	(MOSES, HOME, HOME, name, HOME, name) )
-	run_command("evaluation", command)
+	command = ['%s/scripts/generic/multi-bleu.perl'%(MOSES),
+	 	'%s/mt-dev-test.en'%(HOME)]
+
+	output = run_command("evaluation", command, 
+		stdin=open('%s/output/%s-test.out'%(HOME, name)),
+		stdout=open('%s/output/%s-eval.out'%(HOME, name), 'w'))
+	#with open('%s/output/%s-eval.out'%(HOME, name), 'w') as output_file:
+	#	output_file.write(output)
 
 def main(phrase_len, dist_lim):
 	#gen_phrase_table(phrase_len)
-	name = 's2000-%dpl-%ddl'%(phrase_len, dist_lim)
-
-	outfile.write('\n\nMERT:\n')
-	tune(dist_lim, MERT)
-	decode(name + '-mert')
-	eval(name + '-mert')
+	name = 's50000-%dpl-%ddl'%(phrase_len, dist_lim)	
+	#outfile.write('\n\nMERT:\n')
+	#tune(dist_lim, MERT)
+	#decode(name + '-mert')
+	#eval(name + '-mert')
 
 	outfile.write('\n\nPRO:\n')
 	tune(dist_lim, PRO)
@@ -63,6 +76,8 @@ def main(phrase_len, dist_lim):
 	eval(name + '-pro')
 
 if __name__ == '__main__':
-	outfile = open('moses_output_%s_%s.out'%(sys.argv[1], sys.argv[2]), 'w')
-	main(int(sys.argv[1]), int(sys.argv[2]))
-	outfile.close()
+	phrase_len = 6
+	for dist_lim in [4]:
+		outfile = open('moses_output_50k_%s_%s.out'%(phrase_len, dist_lim), 'w')
+		main(int(phrase_len), int(dist_lim))
+		outfile.close()
