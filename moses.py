60fd5d1ea07431265d66f3df94ee094f5bcbb9af
Kat Busch <kbusch@stanford.edu>
2012-10-10 00:36:29 -0700

    cleaned up script, added README and writeup

--- moses.py
+++ moses.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python
+
 import sys
 import os
 import time
-import commands
 import subprocess
 
 HOME = "/afs/ir/users/k/b/kbusch/cs224n/pa1-mt"
@@ -16,7 +17,6 @@ def run_command(name, command, stdin=None, stdout=None):
 	outfile.write('Command: \n %s' % (command))
 	outfile.flush()
 	start = time.time()
-	#output = commands.getoutput(command)
 	if stdin or stdout:
 		subprocess.call(command, stdin=stdin, stdout=stdout)
 	else:
@@ -34,23 +34,24 @@ def gen_phrase_table(phrase_len):
 		(MOSES, phrase_len, GIZA, HOME, HOME, HOME, HOME) )
 	run_command('generate phrase table', command)
 
-def tune(dist_lim, alg):
+def tune(dist_lim, alg, name):
 	command = ('%s/scripts/training/mert-moses.pl '
-		'--working-dir %s/tune %s '
+		'--working-dir %s/tune-%s %s '
 		'--decoder-flags="-distortion-limit %d -threads 4" %s/mt-dev.fr '
 		'%s/mt-dev.en %s/bin/moses %s/train/model/moses.ini '
 		'--mertdir %s/bin/' %
-		(MOSES, HOME, alg, dist_lim, HOME, HOME, MOSES, HOME, MOSES) )
+		(MOSES, HOME, name, alg, dist_lim, HOME, HOME, MOSES, HOME, MOSES) )
 	run_command("tuning", command) 
 
 def decode(name):
 	command = ['%s/bin/moses'%(MOSES), '-du',
-		'-f', '%s/tune/moses.ini'%(HOME)]
+		'-f', '%s/tune-%s/moses.ini'%(HOME, name)]
 	run_command("decoding", command, 
 		stdin=open('%s/mt-dev-test.fr'%(HOME)),
 		stdout=open('%s/output/%s-test.out'%(HOME, name), 'w'))
 
-	os.system("cp %s/tune/moses.ini %s/output/%s-moses.ini"%(HOME, HOME, name))
+	os.system("cp %s/tune-%s/moses.ini %s/output/%s-moses.ini"%(HOME, 
+								    name, HOME, name))
 
 def eval(name):
 	command = ['%s/scripts/generic/multi-bleu.perl'%(MOSES),
@@ -59,25 +60,31 @@ def eval(name):
 	output = run_command("evaluation", command, 
 		stdin=open('%s/output/%s-test.out'%(HOME, name)),
 		stdout=open('%s/output/%s-eval.out'%(HOME, name), 'w'))
-	#with open('%s/output/%s-eval.out'%(HOME, name), 'w') as output_file:
-	#	output_file.write(output)
 
 def main(phrase_len, dist_lim):
-	#gen_phrase_table(phrase_len)
-	name = 's50000-%dpl-%ddl'%(phrase_len, dist_lim)	
-	#outfile.write('\n\nMERT:\n')
-	#tune(dist_lim, MERT)
-	#decode(name + '-mert')
-	#eval(name + '-mert')
+	name = 'new_tune_s50000-%dpl-%ddl'%(phrase_len, dist_lim)
 
+	gen_phrase_table(phrase_len)
+
+	#MERT		
+	outfile.write('\n\nMERT:\n')
+	tune(dist_lim, MERT, name + '-mert')
+	decode(name + '-mert')
+	eval(name + '-mert')
+
+	#PRO
 	outfile.write('\n\nPRO:\n')
-	tune(dist_lim, PRO)
+	tune(dist_lim, PRO, name + '-pro')
 	decode(name + '-pro')
 	eval(name + '-pro')
 
 if __name__ == '__main__':
-	phrase_len = 6
-	for dist_lim in [4]:
-		outfile = open('moses_output_50k_%s_%s.out'%(phrase_len, dist_lim), 'w')
-		main(int(phrase_len), int(dist_lim))
-		outfile.close()
+	if len(sys.argv) < 3:
+		print "USAGE: moses.py [max phrase len] [dist lim] [path to home folder]"
+	phrase_len = int(sys.argv[1])
+	dist_lim = int(sys.argv[2])
+	if len(sys.argv) > 3:
+		HOME = sys.argv[3]
+	outfile = open('moses_new_tune_50k_%s_%s.out'%(phrase_len, dist_lim), 'w')
+	main(phrase_len, dist_lim)
+	outfile.close()
